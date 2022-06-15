c34390ae6132217e720791b06f7a56d115784136
Fabrizio Poli <Fabrizio_Poli@sluz.ch>
2022-06-15 20:09:15 +0200

    added mail api anf class to send email (dont work)

---
+++ src/main/java/com/app/betnet/MailUtil.java
@@ -0,0 +1,50 @@
+package com.app.betnet;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.Authenticator;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailUtil {
+
+    public static void sendMail(String recepient) throws Exception {
+        Properties properties = new Properties();
+
+        Properties props = new Properties();
+        props.put("mail.smtp.ssl.trust", "*");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        String myAccountEmail = "betnet.help@gmail.com";
+        String password = "sml12345";
+
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+
+        Transport.send(message);
+        System.out.println("Message sent successfully");
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("My First Email from Java App");
+            message.setText("Hey There, \n Look my email");
+            return message;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.app.betnet {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.mail;
 
     opens com.app.betnet to javafx.fxml;
     exports com.app.betnet;
