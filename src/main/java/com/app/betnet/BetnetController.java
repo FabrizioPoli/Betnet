472203c976c54c1e2dd7727354b8e907b19ae9aa
Fabrizio Poli <Fabrizio_Poli@sluz.ch>
2022-06-16 12:36:45 +0200

    updated about us view

--- src/main/java/com/app/betnet/BetnetController.java
+++ src/main/java/com/app/betnet/BetnetController.java
@@ -3,10 +3,7 @@ package com.app.betnet;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.util.Optional;
